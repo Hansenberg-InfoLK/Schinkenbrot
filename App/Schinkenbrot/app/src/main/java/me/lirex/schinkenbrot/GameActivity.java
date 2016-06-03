a3b39f1ab1e149febf57a9d3e9473b7428e13a99
HolyBloodyMary <killerwolke1@hotmail.de>
2016-06-03 11:08:56 +0200

    Weitere Verknüpfungen innerhalb der App & Titel der Activities in der App geändert

--- App/Schinkenbrot/app/src/main/java/me/lirex/schinkenbrot/GameActivity.java
+++ App/Schinkenbrot/app/src/main/java/me/lirex/schinkenbrot/GameActivity.java
@@ -1,5 +1,6 @@
 package me.lirex.schinkenbrot;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -88,27 +89,35 @@ public class GameActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        /**
-         * Handle navigation view item clicks here.
-         */
+        // Die Buttons in der Sidebar werden verlinkt
         int id = item.getItemId();
 
-        /*if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_editor){
+            Intent intent = new Intent(this, EditorActivity.class);
+            startActivity(intent);
+        }
 
-        } else if (id == R.id.nav_slideshow) {
+        if (id == R.id.nav_home){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
 
-        } else if (id == R.id.nav_manage) {
+        if (id == R.id.nav_game){
+            Intent intent = new Intent(this, GameActivity.class);
+            startActivity(intent);
+        }
 
-        } else if (id == R.id.nav_share) {
+        if (id == R.id.nav_host){
+            Intent intent = new Intent (this, HostActivity.class);
+            startActivity(intent);
+        }
 
-        } else if (id == R.id.nav_send) {
-
-        }*/
+        if (id == R.id.nav_settings){
+            Intent intent = new Intent (this, SettingsActivity.class);
+            startActivity(intent);
+        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        assert drawer != null;
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
