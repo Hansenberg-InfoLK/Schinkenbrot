7b9b76bfc73b4284873bfc8cecaf83a215c80167
Niklas Kohlstruck <niklas.kohlstruck@gmail.com>
2016-06-06 12:22:03 +0200

    Update TypeDatasetConnectable.java
    Kurze Beschreibung des Interfaces

--- Core/Virtual_Database/Interfaces/Connectable/Dataset/TypeDatasetConnectable.java
+++ Core/Virtual_Database/Interfaces/Connectable/Dataset/TypeDatasetConnectable.java
@@ -4,6 +4,8 @@ package Interfaces.Connectable.Dataset;
  * @author David Holland
  * Dieses Interface ist von jedem virtuellen Datensatz zu implementieren. So wird eine nachfolgende Funktionsumstellung gewährleistet
  */
+/** Interface der Tabelle "Type" mit den zugehörigen Attributen; wird für die Datenbank (das Einlesen und Speichern der Daten) benötigt
+ */
 public interface TypeDatasetConnectable extends DatasetConnectable
 {
     /* Redundant wegen extends
