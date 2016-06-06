71cf2f86300528afa02590d06894101b0cc05095
Niklas Kohlstruck <niklas.kohlstruck@gmail.com>
2016-06-06 12:18:17 +0200

    Update LanguageDatasetConnectable.java
    Kurze Beschreibung des Interfaces

--- Core/Virtual_Database/Interfaces/Connectable/Dataset/LanguageDatasetConnectable.java
+++ Core/Virtual_Database/Interfaces/Connectable/Dataset/LanguageDatasetConnectable.java
@@ -4,6 +4,10 @@ package Interfaces.Connectable.Dataset;
  * @author David Holland
  * Dieses Interface ist von jedem virtuellen Datensatz zu implementieren. So wird eine nachfolgende Funktionsumstellung gewährleistet
  */
+ 
+ /**
+  * Interface der Tabelle "Language" mit den zugehörigen Attributen; wird für die Datenbank (das Einlesen und Speichern der Daten) benötigt
+  */
 public interface LanguageDatasetConnectable extends DatasetConnectable
 {
     /* Redundant wegen extends
