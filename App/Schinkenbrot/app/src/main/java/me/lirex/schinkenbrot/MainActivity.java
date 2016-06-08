dbf993f4f09c1bbc30370ef40b5456532fc285fa
David Holland <davidholland5499@outlook.com>
2016-06-08 13:04:13 +0200

    Implementierung der GSON Bibliothek zur Verwendung von JSON. Für die Verwendung: import com.google.gson.*;

--- App/Schinkenbrot/app/src/main/java/me/lirex/schinkenbrot/MainActivity.java
+++ App/Schinkenbrot/app/src/main/java/me/lirex/schinkenbrot/MainActivity.java
@@ -11,6 +11,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import com.google.gson.*;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
