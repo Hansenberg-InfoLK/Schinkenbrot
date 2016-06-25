86bc3540a8fdedb6d19e42ac817671016f2c149d
AlZiWi <alex-zimmer@online.de>
2016-06-25 11:02:08 +0200

    API Level zu 16 geändert, Tastatur wird nicht mehr automatisch angezeigt

--- App/Schinkenbrot/app/src/main/java/me/lirex/schinkenbrot/QuestEditorActivity.java
+++ App/Schinkenbrot/app/src/main/java/me/lirex/schinkenbrot/QuestEditorActivity.java
@@ -1,11 +1,14 @@
 package me.lirex.schinkenbrot;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class QuestEditorActivity extends AppCompatActivity {
 
@@ -26,5 +29,32 @@ public class QuestEditorActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    // Diese zwei Methoden überall einbauen, wo etwas gespeichert werden soll.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        // Datenbank Beispiel
+        Context context = getApplicationContext();
+        CharSequence text = "Gespeichert";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+        super.onBackPressed();
     }
 }
